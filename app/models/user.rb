68dd4dd022b89e80e63408fcd0993dfe7f827cb5
Clinton Blackburn <cblackburn@kyru.us>
2014-03-25 16:54:19 -0400

    Added Bootstrap 3.1.1
    Also set root to challenges index page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  def get_display_name
+    display_name || "#{first_name} #{last_name}".strip
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,38 @@ describe User do
     expect(build(:user, first_name: nil)).to_not be_valid
     expect(build(:user, first_name: '')).to_not be_valid
   end
+
+  it 'has a lowercased email field' do
+    email = Faker::Internet.email.upcase!
+    user = create(:user, email: email)
+    expect(user.email).to eq email.downcase
+  end
+
+  describe '#get_display_name' do
+    context 'with display_name set' do
+      let(:user) { build(:user, display_name: Faker::Name.name) }
+
+      it 'returns display_name' do
+        expect(user.get_display_name()).to eq user.display_name
+      end
+    end
+
+    context 'without display_name set' do
+      context 'without last_name set' do
+        let(:user) { build(:user, display_name: nil, last_name: nil) }
+
+        it 'returns first_name' do
+          expect(user.get_display_name()).to eq user.first_name
+        end
+      end
+
+      context 'with last_name set' do
+        let(:user) { build(:user, display_name: nil, last_name: Faker::Name.last_name) }
+
+        it 'returns "first_name last_name"' do
+          expect(user.get_display_name()).to eq "#{user.first_name} #{user.last_name}"
+        end
+      end
+    end
+  end
 end
